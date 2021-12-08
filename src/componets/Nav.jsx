594b77a828f38533ef117fe72b6970134ef45c15
Victoria Cheng <victoria.cheng15@gmail.com>
2021-12-08 18:05:30 -0500

    move images folder to public folder

--- src/componets/Nav.jsx
+++ src/componets/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { CloseOutlined } from '@ant-design/icons';
-import TeslaLogo from '../../images/logo.svg';
+import TeslaLogo from '../../public/images/logo.svg';
 // components
 import Navlink from './nav/Navlink';
 import Button from './button/Button';
